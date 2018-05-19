1633bbf938f76ce441cf3c9eb7d55af86fe00bce
yihwan <yihwankim@gmail.com>
2018-05-18 17:05:57 -0700

    remove console logs

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const inputs = document.querySelectorAll('.controls input');
 
 function handleUpdate(e) {
   const suffix = this.dataset.sizing || '';
-  console.log(e.target.value + suffix);
+
   document.documentElement.style.setProperty(`--${this.name}`, e.target.value + suffix);
 
 }
